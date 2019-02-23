9bd69f688bcd0e890112eb254bdce00771159b8a
opstech23 <bbody8215@gmail.com>
2019-02-23 13:11:49 +0200

    Bootstrap for react, webpack and babel

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import "./App.css"
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+        <h1>Bootstrap Worked</h1>
+          {console.log('Bootstrap Worked!')}
+        </div>
+    )
+  }
+  
+}
+
+export default App
